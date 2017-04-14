3586a3aee531147afa38f9757fc76196c381df70
John <sixtysecondrevit@gmail.com>
2017-04-14 17:22:54 -0600

    Added search DTO

---
+++ Mashy/Mashy/Models/SearchDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mashy.Models
+{
+    public class SearchDTO
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string display_phone { get; set; }
+        public string image_url { get; set; }
+        public double rating { get; set; }
+        public string url { get; set; }
+        public List<string> address { get; set; }
+        public string city { get; set; }
+        public string postal_code { get; set; }
+        public string state_code { get; set; }
+    }
+}
