29634c9d65f5397ba8f7d73bd1323457bd82a8a8
Travis Talcott <ttalcott@aquilagroup.com>
2017-04-14 09:50:11 -0600

    Initial

---
+++ Mashy/Mashy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mashy.MvcApplication" Language="C#" %>
---
+++ Mashy/Mashy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mashy.Startup))]
+namespace Mashy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
