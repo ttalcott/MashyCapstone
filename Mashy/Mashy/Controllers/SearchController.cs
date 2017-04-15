b0ad859f4d194b3d68ddee7ab9d04fbf100c063b
John <sixtysecondrevit@gmail.com>
2017-04-14 18:59:09 -0600

    Added search controller

---
+++ Mashy/Mashy/Controllers/SearchController.cs
@@ -0,0 +1,35 @@
+﻿using Mashy.Controllers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Web.Http;
+
+namespace Mashy.Controllers
+{
+    public class SearchController 
+    {
+        //Get
+        public List<Mashy.Models.YelpSearch> Get()
+        {
+            var model = new List<Mashy.Models.YelpSearch>();
+
+            string request = YelpAPI.PerformRequest();
+
+            DataContractJsonSerializer serialize =
+                new DataContractJsonSerializer(typeof(Mashy.Models.YelpSearch));
+            using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(request)))
+            {
+                var searchResults = (Mashy.Models.YelpSearch)serialize.ReadObject(ms);
+                model.Add(searchResults);
+            }
+
+            return model;
+        }
+
+    }
+}
